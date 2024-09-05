15ea0ed18ff359b29a51646adf054f080d918373
Brenda Hevelyn <brendahevelyn00@gmail.com>
2024-09-05 11:02:36 -0300

    Criado heranca nas classes nokia e iphone, propriedades privativas e sobrescrita.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Console.WriteLine("--------------------");
+Smartphone nokia = new Nokia(numero: "91011", modelo: "Modelo x", imei: "12345", memoria: 555);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone:");
+Console.WriteLine("--------------------");
+Smartphone iphone = new Iphone(numero: "8910", modelo: "Modelo y", imei: "54321", memoria: 444);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
